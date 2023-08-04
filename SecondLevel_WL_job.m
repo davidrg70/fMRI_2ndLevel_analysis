f9983679651b3c884325f0494e56799c38f94cbf
davidrg70 <126163981+davidrg70@users.noreply.github.com>
2023-08-04 22:39:29 +0200

    Add files via upload

---
+++ SecondLevel_WL_job.m
@@ -0,0 +1,78 @@
+% 2nd Level analysis for WL task (2 CONTRASTS: Animals>Fractals & Animals<Fractals)
+clear all;
+analysis_dir = '/home/uni10/nmri/projects/dgarnica/MRI_EEG_PROSPECTIVE/';
+patients_dir = 'ROLANDIC';
+controls_dir = 'CONTROLS';
+func_specification = '/func/1stLevel_wl/optcens/03_both/';
+
+patients_list = {'P.TKMX99A', 'P.WDUK99A', 'P.FVIW99A', 'P.ZGVA99A', 'P.OWYV99A', 'P.NXFW99A', ...
+                 'P.JUQJ99A', 'P.NUSC99A'};
+controls_list = {'C.XWAO99A', 'C.NLPF99A', 'C.ZNOH99A', 'C.PMCH99A', 'C.SLTZ99A', 'C.UYLG99A', ...
+                 'C.ACDS99A', 'C.ZNMG99A', 'C.SDZE99A', 'C.MCPR99A', 'C.DSXW99A', 'C.QVBL99A', ...
+                 'C.IMDS99A', 'C.XBEZ99A', 'C.BUWG99A', 'C.KXOO99A', 'C.XGVX99A'};
+
+contrasts = {'Animals>Fractals', 'Animals<Fractals'};
+contrast_file = 'con_0001.nii,1';
+% NOTE: AS I ASSIGNED 2 CONTRASTS AND THEIR 2 WEIGHTINGS (DOWN), THE con_0002.nii FILES ARE NOT NEEDED!
+
+for p = 1:length(patients_list)
+    patients{p} = fullfile(analysis_dir, patients_dir, patients_list{p}, func_specification, contrast_file);
+end
+patients = patients';
+
+for c = 1:length(controls_list)
+    controls{c} = fullfile(analysis_dir, controls_dir, controls_list{c}, func_specification, contrast_file);
+end
+controls = controls';
+
+%% Run SPM Batch
+clear matlabbatch;
+spm('defaults','fmri');
+spm_jobman('initcfg');
+
+matlabbatch{1}.spm.stats.factorial_design.dir = {'/home/uni10/nmri/projects/dgarnica/MRI_EEG_PROSPECTIVE/SecondLevel_WL'};
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = patients;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.scans2 = controls;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.variance = 1;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.gmsca = 0;
+matlabbatch{1}.spm.stats.factorial_design.des.t2.ancova = 0;
+matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
+matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
+matlabbatch{1}.spm.stats.factorial_design.masking.em = {''};
+matlabbatch{1}.spm.stats.factorial_design.globalc.g_omit = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.gmsca.gmsca_no = 1;
+matlabbatch{1}.spm.stats.factorial_design.globalm.glonorm = 1;
+matlabbatch{2}.spm.stats.fmri_est.spmmat(1) = cfg_dep('Factorial design specification: SPM.mat File', substruct('.','val', '{}',{1}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{2}.spm.stats.fmri_est.write_residuals = 1;
+matlabbatch{2}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{3}.spm.stats.con.spmmat(1) = cfg_dep('Model estimation: SPM.mat File', substruct('.','val', '{}',{2}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.name = contrasts{1};
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.weights = [1 -1];
+matlabbatch{3}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.name = contrasts{2};
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.weights = [-1 1];
+matlabbatch{3}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+
+matlabbatch{3}.spm.stats.con.delete = 0;
+matlabbatch{4}.spm.stats.results.spmmat(1) = cfg_dep('Contrast Manager: SPM.mat File', substruct('.','val', '{}',{3}, '.','val', '{}',{1}, '.','val', '{}',{1}), substruct('.','spmmat'));
+matlabbatch{4}.spm.stats.results.conspec(1).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(1).contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec(1).threshdesc = 'FWE';
+matlabbatch{4}.spm.stats.results.conspec(1).thresh = 0.05;
+matlabbatch{4}.spm.stats.results.conspec(1).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(1).conjunction = 1;
+matlabbatch{4}.spm.stats.results.conspec(1).mask.none = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).titlestr = '';
+matlabbatch{4}.spm.stats.results.conspec(2).contrasts = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).threshdesc = 'FWE';
+matlabbatch{4}.spm.stats.results.conspec(2).thresh = 0.05;
+matlabbatch{4}.spm.stats.results.conspec(2).extent = 0;
+matlabbatch{4}.spm.stats.results.conspec(2).conjunction = 1;
+matlabbatch{4}.spm.stats.results.conspec(2).mask.none = 1;
+matlabbatch{4}.spm.stats.results.units = 1;
+matlabbatch{4}.spm.stats.results.export{1}.ps = true;
+
+spm_jobman('run', matlabbatch);
