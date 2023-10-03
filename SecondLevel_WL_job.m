73364bfae1d39e654ef3341b2823a4a504a0163b
davidrg70 <126163981+davidrg70@users.noreply.github.com>
2023-10-03 21:52:06 +0200

    Deal with covariates

--- SecondLevel_WL_job.m
+++ SecondLevel_WL_job.m
@@ -25,13 +25,30 @@ for c = 1:length(controls_list)
 end
 controls = controls';
 
+%% Deal with covariates
+% Load covariates - Age, sex (zeros for females, ones for males), and handedness (zeros for left-handers, ones for right-handers)
+load([analysis_dir,'SecondLevel_WL','/','covariates.mat']);
+% Check for significant differences between groups in covariates
+% first, distinguish patients from controls
+for i = 1:size(covariates,1)
+    if contains(covariates.Pseudonym(i), 'P.')
+        log_subjects(i) = 0;
+    elseif contains(covariates.Pseudonym(i), 'C.')
+        log_subjects(i) = 1;
+    end
+end
+log_subjects = log_subjects'; % zeros for patients, ones for controls
+Age_patients = covariates.Age(log_subjects == 0); Sex_patients = covariates.Sex(log_subjects == 0); Hand_patients = covariates.Handedness(log_subjects == 0);
+Age_controls = covariates.Age(log_subjects == 1); Sex_controls = covariates.Sex(log_subjects == 1); Hand_controls = covariates.Handedness(log_subjects == 1);
+% Compare, using Wilcoxon rank sum test (equivalent to Mann-Whitney U test)
+[p_age,h_age,stats_age] = ranksum(Age_patients, Age_controls);
+[p_sex,h_sex,stats_sex] = ranksum(Sex_patients, Sex_controls);
+[p_hand,h_hand,stats_hand] = ranksum(Hand_patients, Hand_controls);
+
 %% Run SPM Batch
 clear matlabbatch;
 spm('defaults','fmri');
 spm_jobman('initcfg');
-
-% Load covariates - Age, sex (zeros for females, ones for males), and handedness (zeros for left-handers, ones for right-handers)
-load([analysis_dir,'SecondLevel_WL','/','covariates.mat']);
 
 matlabbatch{1}.spm.stats.factorial_design.dir = {'/home/uni10/nmri/projects/dgarnica/MRI_EEG_PROSPECTIVE/SecondLevel_WL'};
 matlabbatch{1}.spm.stats.factorial_design.des.t2.scans1 = patients;
@@ -40,18 +57,67 @@ matlabbatch{1}.spm.stats.factorial_design.des.t2.dept = 0;
 matlabbatch{1}.spm.stats.factorial_design.des.t2.variance = 1;
 matlabbatch{1}.spm.stats.factorial_design.des.t2.gmsca = 0;
 matlabbatch{1}.spm.stats.factorial_design.des.t2.ancova = 0;
-matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Age];
-matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Age';
-matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1;
-matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
-matlabbatch{1}.spm.stats.factorial_design.cov(2).c = [covariates.Sex];
-matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = 'Sex';
-matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
-matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
-matlabbatch{1}.spm.stats.factorial_design.cov(3).c = [covariates.Handedness];
-matlabbatch{1}.spm.stats.factorial_design.cov(3).cname = 'Handedness';
-matlabbatch{1}.spm.stats.factorial_design.cov(3).iCFI = 1;
-matlabbatch{1}.spm.stats.factorial_design.cov(3).iCC = 1;
+
+% Add covariates to the model if there are significant differences between groups
+if p_age < 0.05 && p_sex < 0.05 && p_hand < 0.05                            % if all are sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Age]; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Age';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).c = [covariates.Sex];
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = 'Sex';
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(3).c = [covariates.Handedness];
+    matlabbatch{1}.spm.stats.factorial_design.cov(3).cname = 'Handedness';
+    matlabbatch{1}.spm.stats.factorial_design.cov(3).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(3).iCC = 1;
+elseif p_age < 0.05 && p_sex < 0.05                                         % if age and sex are sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Age]; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Age';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).c = [covariates.Sex];
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = 'Sex';
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
+elseif p_age < 0.05 && p_hand < 0.05                                        % if age and handedness are sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Age]; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Age';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).c = [covariates.Handedness];
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = 'Handedness';
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
+elseif p_sex < 0.05 && p_hand < 0.05                                        % if sex and handedness are sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Sex];
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Sex';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).c = [covariates.Handedness];
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).cname = 'Handedness';
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(2).iCC = 1;
+elseif p_age < 0.05                                                         % if only age is sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Age]; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Age';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1; 
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+elseif p_sex < 0.05                                                         % if only sex is sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Sex];
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Sex';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+elseif p_hand < 0.05                                                        % if only handedness is sign. different
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).c = [covariates.Handedness];
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).cname = 'Handedness';
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCFI = 1;
+    matlabbatch{1}.spm.stats.factorial_design.cov(1).iCC = 1;
+else                                                                        % if any is sign. different 
+    matlabbatch{1}.spm.stats.factorial_design.cov = struct('c', {}, 'cname', {}, 'iCFI', {}, 'iCC', {});
+end
+
 matlabbatch{1}.spm.stats.factorial_design.multi_cov = struct('files', {}, 'iCFI', {}, 'iCC', {});
 matlabbatch{1}.spm.stats.factorial_design.masking.tm.tm_none = 1;
 matlabbatch{1}.spm.stats.factorial_design.masking.im = 1;
